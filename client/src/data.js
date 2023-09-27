7e4e79819027e42a24ad94262e5ca9167f01cafb
Rajesh295-dev <{rajeshgautam766@yahoo.com}+{Rajesh295-dev}@users.noreply.github.com>
2023-09-27 11:17:53 -0400

    user added 🍏🍏

--- client/src/data.js
+++ client/src/data.js
@@ -1,16 +1,25 @@
 export const posts = [
     {
         id: 1,
-        username: "john",
-        fullname: "John Keller",
-        userImg: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        username: "matt",
+        fullname: "Matt Bataglia",
+        userImg: "https://media.gq.com/photos/62472b8cd3c1606db058cfc6/16:9/w_4206,h_2366,c_limit/GettyImages-534354204.jpeg",
         postImg: "https://www.hubspot.com/hubfs/good-pictures-phone-tips.jpg",
     },
     {
         id: 2,
-        username: "monica",
-        fullname: "Monica Stan",
-        userImg: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        username: "ashley",
+        fullname: "Ashley Somers",
+        userImg: "https://i.pinimg.com/736x/f3/f1/cc/f3f1cc83330f1a821694bd781ae2c159.jpg",
         postImg: "https://a.storyblok.com/f/90417/1050x700/97946696d7/photo-1591035897819-f4bdf739f446.webp",
     },
+
+    {
+        id: 3,
+        username: "robin",
+        fullname: "Robin Stella",
+        userImg: "https://www.enjpg.com/img/2020/cool-pictures-3.jpg",
+        postImg: "https://rare-gallery.com/uploads/posts/506314-lion-free-high.jpg",
+
+    }
 ];
