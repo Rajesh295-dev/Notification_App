012be0b6febb82d8b56a2179195662878a37f980
Rajesh295-dev <{rajeshgautam766@yahoo.com}+{Rajesh295-dev}@users.noreply.github.com>
2023-09-26 22:12:34 -0400

    initial commit 🍏🍏

--- client/src/data.js
+++ client/src/data.js
@@ -4,13 +4,13 @@ export const posts = [
         username: "john",
         fullname: "John Keller",
         userImg: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        postImg: "https://images.pexels.com/photos/9730025/pexels-photo-9730025.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        postImg: "https://www.hubspot.com/hubfs/good-pictures-phone-tips.jpg",
     },
     {
         id: 2,
         username: "monica",
         fullname: "Monica Stan",
         userImg: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        postImg: "https://images.pexels.com/photos/3497624/pexels-photo-3497624.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        postImg: "https://a.storyblok.com/f/90417/1050x700/97946696d7/photo-1591035897819-f4bdf739f446.webp",
     },
 ];
